5d9f7d5d339f648af60dcbaa78eefd51c5fc5706
iraaiyani <nurathirahaiyani@gmail.com>
2025-02-17 15:30:43 +0800

    Create  script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// You can add more JavaScript functionality here
